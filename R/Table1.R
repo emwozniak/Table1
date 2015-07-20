d95167c3668a65d983b89dc67d9bd781a49a6842
ERICA WOZNIAK <EWOZNIAK@MCW.EDU>
2015-07-20 14:02:03 -0500

    Revise cat.var to call stat.col for p-values instead of computing them internally.

--- R/Table1.R
+++ R/Table1.R
@@ -16,15 +16,15 @@ cat.var <- function(var,
     n <- length(which(is.na(var)==FALSE))
     tot <- as.matrix(table(var))
     tot[] <- paste0(tot, 
-                paste0(' (', 
-                    format(round((tot/colSums(tot))*100, dec), 
-                    nsmall=dec), '%)'))
+                    paste0(' (', 
+                           format(round((tot/colSums(tot))*100, dec), 
+                                  nsmall=dec), '%)'))
     miss <- length(which(is.na(var)==T))
     out <- sapply(data.frame(rbind(rep(NA, length(levels(as.factor(strat))) + 1), 
-                                n, 
-                                rep(NA, length(levels(as.factor(strat))) + 1),
-                                tot, 
-                                miss),
+                                   n, 
+                                   rep(NA, length(levels(as.factor(strat))) + 1),
+                                   tot, 
+                                   miss),
                              row.names=NULL),
                   as.character)
     out <- replace(out, is.na(out), '')
@@ -42,30 +42,30 @@ cat.var <- function(var,
     n <- as.matrix(apply(cat, 2, sum))
     n[] <- paste0(n,
                   paste0(' (',
-                      format(round((n/sum(n))*100, dec),
-                      nsmall=dec), '%)')
-                  )
+                         format(round((n/sum(n))*100, dec),
+                                nsmall=dec), '%)')
+    )
     n <- cbind(t(n), apply(tot, 2, sum))
     
     #Stratified summaries
     cat[] <- paste0(cat, 
                     paste0(' (', 
-                          format(round((cat/rowSums(cat))*100, dec), 
-                          nsmall=dec), '%)',
-                    paste0(' (', 
-                          format(round(t(t(cat)/colSums(cat))*100, dec), 
-                          nsmall=dec), '%)')
+                           format(round((cat/rowSums(cat))*100, dec), 
+                                  nsmall=dec), '%)',
+                           paste0(' (', 
+                                  format(round(t(t(cat)/colSums(cat))*100, dec), 
+                                         nsmall=dec), '%)')
                     ))
     
     #Overall summary for total column
     tot[] <- paste0(tot, 
                     paste0(' (', 
-                          format(round((tot/rowSums(tot))*100, dec), 
-                          nsmall=dec), '%)',
-                    paste0(' (', 
-                          format(round((tot/colSums(tot))*100, dec), 
-                          nsmall=dec), '%)'))
-                    )
+                           format(round((tot/rowSums(tot))*100, dec), 
+                                  nsmall=dec), '%)',
+                           paste0(' (', 
+                                  format(round((tot/colSums(tot))*100, dec), 
+                                         nsmall=dec), '%)'))
+    )
     miss <- c(aggregate(var, list(strat), function(x) {sum(is.na(x))})[,-1],
               sum(aggregate(var, list(strat), function(x) {sum(is.na(x))})[,-1]))
     
@@ -85,115 +85,24 @@ cat.var <- function(var,
     colnames(out) <- c('Variable', as.vector(levels(as.factor(strat))), 'Overall')
   }
   
-  #Chi-square test
-  ##Options to print test on table
-  ##Re-format small p-values
-  if (ptype=='chisq') {
-    p <- chisq.test(var, strat)$p.value
-    if (p>=0.0001 & pname==TRUE) {
-      p.col <- c(signif(p, 3), 'Chi-square', rep(NA, length(levels(as.factor(var))) + 2))
-    }
-    else if (p<0.0001 & pname==TRUE) {
-      p.col <- c('<0.0001', 'Chi-square', rep(NA, length(levels(as.factor(var))) + 2))
-    }
-    else if (p>=0.0001 & pname==FALSE) {
-      p.col <- c(signif(p, 3), rep(NA, length(levels(as.factor(var))) + 3)) 
-    }
-    else if (p<0.0001 & pname==FALSE) {
-      p.col <- c('<0.0001', rep(NA, length(levels(as.factor(var))) + 3))
-    }
-    
-    #Create data frame with chi-square p-values          
-    out <- sapply(data.frame(cbind(rbind(rep(NA, length(levels(as.factor(strat))) + 1),
-                                         n,
-                                         rep(NA, length(levels(as.factor(strat))) + 1),
-                                         cbind(cat, tot), 
-                                         miss),
-                                   p.col),
-                             row.names=NULL),
-                  as.character)
+  if (ptype!='None' & pname==FALSE) {
+    p <- c(stat.col(var, strat, ptype, pname=FALSE), rep(NA, length(levels(as.factor(var))) + 3))
+    out <- cbind(out, p)
     out <- replace(out, is.na(out), '')
-    out <- cbind(as.vector(c(paste(header, '     '), '  Count (%)', '  (Row %)(Col %)', 
-                             paste0('   ', rownames), '  Missing')), out)
+    rownames(out) <- NULL
+    colnames(out) <- c('Variable', as.vector(levels(as.factor(strat))), 'Overall', 'p-value')
+    
+  }
+  
+  else if (ptype!='None' & pname==TRUE) {
+    p <- c(stat.col(var, strat, ptype, pname=TRUE), rep(NA, length(levels(as.factor(var))) + 2))
+    out <-  cbind(out, p)
+    out <- replace(out, is.na(out), '')
     rownames(out) <- NULL
     colnames(out) <- c('Variable', as.vector(levels(as.factor(strat))), 'Overall', 'p-value')
   }
-  
-  #Fisher exact test
-  ##Options to print test on table
-  ##Re-format small p-values
-  else {
-    if (ptype=='fisher') {
-      p <- fisher.test(var, strat)$p.value
-      if (p>=0.0001 & pname==TRUE) {
-        p.col <- c(signif(p, 3), 'Fisher exact', rep(NA, length(levels(as.factor(var))) + 2))
-      }
-      else if (p<0.0001 & pname==TRUE) {
-        p.col <- c('<0.0001', 'Fisher exact', rep(NA, length(levels(as.factor(var))) + 2))
-      }
-      else if (p>=0.0001 & pname==FALSE) {
-        p.col <- c(signif(p, 3), rep(NA, length(levels(as.factor(var))) + 3))
-      }
-      else if (p<0.0001 & pname==FALSE) {
-        p.col <- c('<0.0001', rep(NA, length(levels(as.factor(var))) + 3))
-      }
-      
-      
-      #Create data frame with Fisher test p-values       
-      out <- sapply(data.frame(cbind(rbind(rep(NA, length(levels(as.factor(strat))) + 1),
-                                           n,
-                                           rep(NA, length(levels(as.factor(strat))) + 1),
-                                           cbind(cat, tot), 
-                                           miss),
-                                     p.col),
-                               row.names=NULL),
-                    as.character)
-      out <- replace(out, is.na(out), '')
-      out <- cbind(as.vector(c(paste(header, '     '), '  Count (%)', '  (Row %)(Col %)', paste0('   ', rownames), '   Missing')), out)
-      rownames(out) <- NULL
-      colnames(out) <- c('Variable', as.vector(levels(as.factor(strat))), 'Overall', 'p-value')
-    }
-    
-    #McNemar's test
-    ##Options to print test on table
-    ##Re-format small p-values
-    else {
-      if (ptype=='mcnemar') {
-        p <- mcnemar.test(var, strat)$p.value
-        if (p>=0.0001 & pname==TRUE) {
-          p.col <- c(signif(p, 3), 'McNemar', rep(NA, length(levels(as.factor(strat))) + 2))
-        }
-        else if (p<0.0001 & pname==TRUE) {
-          p.col <- c('<0.0001', 'McNemar', rep(NA, length(levels(as.factor(strat))) + 2))
-        }
-        else if (p>=0.0001 & pname==FALSE) {
-          p.col <- c(signif(p, 3), rep(NA, length(levels(as.factor(strat))) + 3))
-        }
-        else if (p<0.0001 & pname==FALSE) {
-          p.col <- c('<0.0001', rep(NA, length(levels(as.factor(strat))) + 3))
-        }
-        
-        
-        #Create data frame with McNemar's test p-values        
-        out <- sapply(data.frame(cbind(rbind(rep(NA, length(levels(as.factor(strat))) + 1),
-                                             n,
-                                             rep(NA, length(levels(as.factor(strat))) + 1),
-                                             cbind(cat, tot), 
-                                             miss),
-                                       p.col),
-                                 row.names=NULL),
-                      as.character)
-        out <- replace(out, is.na(out), '')
-        out <- cbind(as.vector(c(paste(header, '     '), '  Count (%)', '  (Row %)(Col %)', 
-                                 paste0('   ', rownames), '   Missing')), out)
-        rownames(out) <- NULL
-        colnames(out) <- c('Variable', as.vector(levels(as.factor(strat))), 'Overall', 'p-value')
-      }
-    }
-  }
   return(data.frame(out))
 }
-
 
 ##############
 # Continuous #
